7394c2ddf49c43de4d8c99e2b79a8c5fe511ab5c
Jason Stevens <thinkjrs@gmail.com>
2019-02-16 17:00:46 -0600

    separate and add tests for both utilities and credentials

---
+++ tests/test_credentials.py
@@ -0,0 +1,74 @@
+"""
+Unit tests for credentials.py within pycm.
+"""
+import pytest
+import json
+import sys
+import os
+import requests
+import pycm.utilities as utilities
+import pycm.credentials as credentials
+
+@pytest.fixture
+def projpath(path=None):
+    if path is not None:
+        if path[-1] != '/': # add trailing slash
+            path += '/'
+        return path
+    return utilities.ProjectRootDir()
+
+@pytest.fixture
+def credential(projpath):
+    # load credentials
+    with open(f"{projpath}/.credentials.json") as fp:
+        credential = json.load(fp)
+    return credential
+
+@pytest.fixture
+def credentials_response(credential):
+    """
+    Make a request to the charmetric api for the access token and
+    return the text of the response.
+    """
+    authURL = f"https://api.chartmetric.io/api/token"
+    headers = {'Content-Type': 'application/json',}
+    refreshtokenkey = 'refreshtoken'
+    refreshtoken = credential[refreshtokenkey]
+    data = '{' + f'"{refreshtokenkey}":"{refreshtoken}"' + '}'
+
+    response = requests.post(authURL, headers=headers, data=data)
+  
+    if not response.ok: # raise if issue
+        response.raise_for_status()
+    return response.text
+ 
+def test_CredentialsFilename():
+    assert credentials.Filename() == '.credentials.json'
+
+def test_CheckCredentials():
+    assert credentials.Check() == True
+
+def test_LoadCredentials(credential):
+    assert credential == credentials.Load()
+
+def test_FetchAccessToken(credentials_response):
+    # bad response will throw within FetchAccessToken (and will raise
+    # within the test runner execution)
+    assert credentials_response is not None
+    assert credentials_response != ''
+    assert credentials.Load()['refreshtoken'] == credentials.FetchAccessToken()['refresh_token']
+
+def test_UpdateCredentials():
+    credentials.Update()
+    assert os.path.exists(utilities.ProjectRootDir()
+               + credentials.Filename()
+           )
+    assert credentials.Load()['scope'] != ''
+    assert credentials.Load()['token'] != ''
+    assert credentials.Load()['expires_in'] != ''
+
+def test_PeriodicCredentials():
+    stale = credentials.Load()
+    credentials.PeriodicUpdate(1) # wait a single second
+    new = credentials.Load()
+    assert new['token'] != stale['token']
--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -16,57 +16,6 @@ def projpath(path=None):
         return path
     return utilities.ProjectRootDir()
 
-@pytest.fixture
-def credentials(projpath):
-    # load credentials
-    with open(f"{projpath}/.credentials.json") as fp:
-        credentials = json.load(fp)
-    return credentials 
-
-@pytest.fixture
-def credentials_response(credentials):
-    """
-    Make a request to the charmetric api for the access token and
-    return the text of the response.
-    """
-    authURL = f"https://api.chartmetric.io/api/token"
-    headers = {'Content-Type': 'application/json',}
-    refreshtokenkey = 'refreshtoken'
-    refreshtoken = credentials[refreshtokenkey]
-    data = '{' + f'"{refreshtokenkey}":"{refreshtoken}"' + '}'
-
-    response = requests.post(authURL, headers=headers, data=data)
-  
-    if not response.ok: # raise if issue
-        response.raise_for_status()
-    return response.text
- 
-def test_CredentialsFilename():
-    assert utilities.CredentialsFilename() == '.credentials.json'
-
-def test_CheckCredentials():
-    assert utilities.CheckCredentials() == True
-
-def test_LoadCredentials(credentials):
-    assert credentials == utilities.LoadCredentials()
-
-def test_FetchAccessToken(credentials_response):
-    # bad response will throw within FetchAccessToken (and will raise
-    # within the test runner execution)
-    assert credentials_response is not None
-    assert credentials_response != ''
-    assert utilities.LoadCredentials()['refreshtoken'] == utilities.FetchAccessToken()['refresh_token']
-
-def test_UpdateCredentials():
-    utilities.UpdateCredentials()
-    assert os.path.exists(utilities.ProjectRootDir() + '.credentials.json')
-    assert utilities.LoadCredentials()['scope'] != ''
-    assert utilities.LoadCredentials()['token'] != ''
-    assert utilities.LoadCredentials()['expires_in'] != ''
-
-def test_PeriodicCredentials():
-    pass
-
 def test_projectrootdir():
     """
     NOTE: Test *should* work on other systems but be weary...
