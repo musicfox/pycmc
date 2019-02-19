d8b75c9394bb1ef317cd553f2ac5f913d6a9b41a
Jason R. Stevens <thinkjrs@gmail.com>
2019-02-19 00:33:36 -0600

    add basic setup for pip installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pycm', 
+      version='0.1',
+      description='A Python client for the chartmetric.io API.',
+      url='https://github.com:thinkjrs/pycm',
+      author='Jason R. Stevens, CFA',
+      author_email='jason@thinkjrs.org',
+      license='MIT',
+      packages=['pycm'],
+      zip_safe=False,
+)
