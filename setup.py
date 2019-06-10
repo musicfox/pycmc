c651eb3e37b4d4721d123d70da1302bd9f480834
Jason R. Stevens <thinkjrs@gmail.com>
2019-06-10 13:12:59 -0500

    update version for :package:

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/thinkjrs/pycm'
 EMAIL = 'jason@thinkjrs.org'
 AUTHOR = 'Jason R. Stevens, CFA'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.1.0'
+VERSION = '0.1.01'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
