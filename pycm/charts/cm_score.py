bfe26d520b6cefd07db52ff5202b404435d58a88
Richard Shu <richard@musicfox.io>
2019-08-22 22:40:07 +0000

    halfway commit proof-reading cm_score.py

--- pycm/charts/cm_score.py
+++ pycm/charts/cm_score.py
@@ -3,20 +3,18 @@ from .. import utilities
 
 def tracks(cm_track_id, chart_type, start_date, end_date=None):
     """
-    Query the charts/amazon/tracks endpoint for the
-    given date.
+    Gets the Chartmetric Score for tracks given the CM track ID.
 
-    :param cm_track_id: Chartmetric track ID
-    :param chart_type:  string chart type, taking one of
-                        {'spotify-top', 'spotify-viral',
-                        'applemusic-genre', 'applemusic-daily', 
-                        'applemusic-albums' 'itunes', 
-                        'itunes-albums', 'shazam}
-    :param start_date:  string date in ISO format %Y-%m-%y
-    :param end_date:    string date in ISO format %Y-%m-%y, default today
+    :param cm_track_id:     Chartmetric track ID
+    :param chart_type:      string chart type, choose from
+                            'spotify-top', 'spotify-viral',
+                            'applemusic-genre', 'applemusic-daily', 
+                            'applemusic-albums' 'itunes', 
+                            'itunes-albums', 'shazam'
+    :param start_date:      string date in ISO format %Y-%m-%y
+    :param end_date:        string date in ISO format %Y-%m-%y, default today
 
-    :returns:           list of dictionary of amazon tracks
-                        chart data
+    :returns:               list of CM scores
     """
     urlhandle = f"/charts/track/{cm_track_id}/{chart_type}/cm-score"
     params = {
@@ -30,20 +28,18 @@ def tracks(cm_track_id, chart_type, start_date, end_date=None):
 
 def artists(cm_artist_id, chart_type, start_date, end_date=None):
     """
-    Query the charts/amazon/tracks endpoint for the
-    given date.
+    Gets the Chartmetric Score for artists given the CM artist ID.
 
-    :param cm_artist_id: Chartmetric artist ID
-    :param chart_type:   string chart type, taking one of
-                         {'spotify-top', 'spotify-viral',
-                         'applemusic-genre', 'applemusic-daily', 
-                         'applemusic-albums' 'itunes', 
-                         'itunes-albums', 'shazam}
-    :param start_date:   string date in ISO format %Y-%m-%y
-    :param end_date:     string date in ISO format %Y-%m-%y, default today
+    :param cm_artist_id:    Chartmetric artist ID
+    :param chart_type:      string chart type, choose from
+                            'spotify-top', 'spotify-viral',
+                            'applemusic-genre', 'applemusic-daily', 
+                            'applemusic-albums' 'itunes', 
+                            'itunes-albums', 'shazam'
+    :param start_date:      string date in ISO format %Y-%m-%y
+    :param end_date:        string date in ISO format %Y-%m-%y, default today
 
-    :returns:            list of dictionary of amazon tracks
-                         chart data
+    :returns:               list of CM scores
     """
     urlhandle = f"/charts/artist/{cm_artist_id}/{chart_type}/cm-score"
     params = {
@@ -52,25 +48,23 @@ def artists(cm_artist_id, chart_type, start_date, end_date=None):
     }
         
     data = utilities.RequestData(urlhandle, params)
-    return utilities.RequestGet(data)['data']
+    return utilities.RequestGet(data)
 
 
 def albums(cm_album_id, chart_type, start_date, end_date=None):
     """
-    Query the charts/amazon/tracks endpoint for the
-    given date.
+    Gets the Chartmetric Score for albums given the CM album ID.
 
-    :param cm_album_id: Chartmetric album ID
-    :param chart_type:  string chart type, taking one of
-                        {'spotify-top', 'spotify-viral',
-                        'applemusic-genre', 'applemusic-daily', 
-                        'applemusic-albums' 'itunes', 
-                        'itunes-albums', 'shazam}
-    :param start_date:  string date in ISO format %Y-%m-%y
-    :param end_date:    string date in ISO format %Y-%m-%y, default today
+    :param cm_album_id:     Chartmetric album ID
+    :param chart_type:      string chart type, choose from
+                            'spotify-top', 'spotify-viral',
+                            'applemusic-genre', 'applemusic-daily', 
+                            'applemusic-albums' 'itunes', 
+                            'itunes-albums', 'shazam'
+    :param start_date:      string date in ISO format %Y-%m-%y
+    :param end_date:        string date in ISO format %Y-%m-%y, default today
 
-    :returns:           list of dictionary of amazon tracks
-                        chart data
+    :returns:               list of CM scores
     """
     urlhandle = f"/charts/album/{cm_album_id}/{chart_type}/cm-score"
     params = {
