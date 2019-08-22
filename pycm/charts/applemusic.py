dd6aa5c3e47eeae25adf5a38aa131159b58971b4
Richard Shu <richard@musicfox.io>
2019-08-22 22:11:17 +0000

    finish proof-reading applemusic.py

--- pycm/charts/applemusic.py
+++ pycm/charts/applemusic.py
@@ -4,8 +4,9 @@ charts_apple_music_url = f"/charts/applemusic"
 
 def tracks(date, country='US', genre="All Genres"):
     """
-    Query the charts/apple_music/tracks endpoint for the
-    given date.
+    Query the charts/apple_music/tracks endpoint for the given date.
+
+    https://api.chartmetric.com/api/charts/applemusic/tracks
 
     :param date:        string date in ISO format %Y-%m-%y
     :param country:     string country code, e.g. 'US'
@@ -27,8 +28,9 @@ def tracks(date, country='US', genre="All Genres"):
 
 def albums(date, country="US", genre="All Genres"):
     """
-    Query the charts/apple_music/albums endpoint for the
-    given date.
+    Query the charts/apple_music/albums endpoint for the given date.
+
+    https://api.chartmetric.com/api/charts/applemusic/albums
 
     :param date:        string date in ISO format %Y-%m-%y
     :param country:     string country code, e.g. 'US'
@@ -48,8 +50,9 @@ def albums(date, country="US", genre="All Genres"):
 
 def videos(date, country="US", genre="All Genres"):
     """
-    Query the charts/apple_music/videos endpoint for the
-    given date.
+    Query the charts/apple_music/videos endpoint for the given date.
+
+    https://api.chartmetric.com/api/charts/applemusic/videos
 
     :param date:        string date in ISO format %Y-%m-%y
     :param country:     string country code, e.g. 'US'
