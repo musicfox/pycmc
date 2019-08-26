a7606ec64f7fbe19617eb1a73dc41dd0bb1454c1
Richard Shu <richard@musicfox.io>
2019-08-26 15:46:40 +0000

    finish cleaning up qq

--- pycm/charts/qq.py
+++ pycm/charts/qq.py
@@ -1,12 +1,11 @@
 """
-Defines methods for obtaining QQ chart data from the 
-chartmetric.com API.
+Defines methods for obtaining QQ chart data from the chartmetric API.
 
 Usage:
 ======
 
 >>> from pycm.charts import qq
->>> qq.insights('2019-01-01') # who knows what you get back!
+>>> qq.insights('2019-01-01') # only available on Thursdays
 
 """
 from .. import utilities
@@ -15,12 +14,14 @@ qq_music_url = f"/charts/qq"
 
 def insights(date):
     """
-    Query the charts/qq/ endpoint for the
-    given date.
+    Get the top tracks on QQ music chart for the given date.
+    Data ONLY available on Thursdays.
 
-    :param date:        string date in ISO format %Y-%m-%y
+    https://api.chartmetric.com/api/charts/qq/
 
-    :returns:           list of dictionary of QQ music data insights
+    :param date:        string date in ISO format %Y-%m-%d
+
+    :return:            list of dictionary of tracks on QQ music chart
     """
     urlhandle = f"{qq_music_url}/"
     params = {
