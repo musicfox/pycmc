a7c1a78153e2cb74060ca3a6d1b152875c066b70
Richard Shu <richard@musicfox.io>
2019-08-26 15:01:34 +0000

    finish cleaning up beatport

--- pycm/charts/beatport.py
+++ pycm/charts/beatport.py
@@ -5,12 +5,13 @@ beatport_charts_url = f"/charts/beatport"
 def tracks(date):
     """
     Query the charts/beatport/tracks endpoint for the given date.
+    Data available ONLY on Fridays.
 
     https://api.chartmetric.com/api/charts/beatport
 
-    :param date:        string date in ISO format %Y-%m-%y
+    :param date:        string date in ISO format %Y-%m-%d
 
-    :returns:           list of dictionary of Beatport chart data
+    :returns:           list of dictionary of tracks on Beatport charts
     """
     urlhandle = f"{beatport_charts_url}"
     params = {
