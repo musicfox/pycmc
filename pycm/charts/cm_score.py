f0ac456fe868b8bc4f7348dba731123213840b1c
Richard Shu <richard@musicfox.io>
2019-08-26 15:24:36 +0000

    finish cleaning up cm_score

--- pycm/charts/cm_score.py
+++ pycm/charts/cm_score.py
@@ -5,16 +5,18 @@ def tracks(cm_track_id, chart_type, start_date, end_date=None):
     """
     Gets the Chartmetric Score for tracks given the CM track ID.
 
+    https://api.chartmetric.com/api/charts/track/:type_id/:chart_type/cm-score
+
     :param cm_track_id:     Chartmetric track ID
     :param chart_type:      string chart type, choose from
                             'spotify-top', 'spotify-viral',
                             'applemusic-genre', 'applemusic-daily', 
                             'applemusic-albums' 'itunes', 
                             'itunes-albums', 'shazam'
-    :param start_date:      string date in ISO format %Y-%m-%y
-    :param end_date:        string date in ISO format %Y-%m-%y, default today
+    :param start_date:      string date in ISO format %Y-%m-%d
+    :param end_date:        string date in ISO format %Y-%m-%d, default today
 
-    :returns:               list of CM scores
+    :returns:               list of dictionary of CM scores for the track
     """
     urlhandle = f"/charts/track/{cm_track_id}/{chart_type}/cm-score"
     params = {
@@ -30,16 +32,18 @@ def artists(cm_artist_id, chart_type, start_date, end_date=None):
     """
     Gets the Chartmetric Score for artists given the CM artist ID.
 
+    https://api.chartmetric.com/api/charts/artist/:type_id/:chart_type/cm-score
+
     :param cm_artist_id:    Chartmetric artist ID
     :param chart_type:      string chart type, choose from
                             'spotify-top', 'spotify-viral',
                             'applemusic-genre', 'applemusic-daily', 
                             'applemusic-albums' 'itunes', 
                             'itunes-albums', 'shazam'
-    :param start_date:      string date in ISO format %Y-%m-%y
-    :param end_date:        string date in ISO format %Y-%m-%y, default today
+    :param start_date:      string date in ISO format %Y-%m-%d
+    :param end_date:        string date in ISO format %Y-%m-%d, default today
 
-    :returns:               list of CM scores
+    :returns:               list of dictionary of CM scores for the artist
     """
     urlhandle = f"/charts/artist/{cm_artist_id}/{chart_type}/cm-score"
     params = {
@@ -55,16 +59,18 @@ def albums(cm_album_id, chart_type, start_date, end_date=None):
     """
     Gets the Chartmetric Score for albums given the CM album ID.
 
+    https://api.chartmetric.com/api/charts/album/:type_id/:chart_type/cm-score
+
     :param cm_album_id:     Chartmetric album ID
     :param chart_type:      string chart type, choose from
                             'spotify-top', 'spotify-viral',
                             'applemusic-genre', 'applemusic-daily', 
                             'applemusic-albums' 'itunes', 
                             'itunes-albums', 'shazam'
-    :param start_date:      string date in ISO format %Y-%m-%y
-    :param end_date:        string date in ISO format %Y-%m-%y, default today
+    :param start_date:      string date in ISO format %Y-%m-%d
+    :param end_date:        string date in ISO format %Y-%m-%d, default today
 
-    :returns:               list of CM scores
+    :returns:               list of dictionary of CM scores for the album
     """
     urlhandle = f"/charts/album/{cm_album_id}/{chart_type}/cm-score"
     params = {
