d9922e5245ac247dda82e509e1431abfc9a6bf38
Richard Shu <richard@musicfox.io>
2019-08-26 15:34:59 +0000

    finish cleaning up itunes

--- pycm/charts/itunes.py
+++ pycm/charts/itunes.py
@@ -4,14 +4,15 @@ itunes_charts_url = f"/charts/itunes"
 
 def albums(date, country="US"):
     """
-    Query the charts/itunes/albums endpoint for the given date.
+    Get the top 200 albums on iTunes charts for the given date.
 
     https://api.chartmetric.com/api/charts/itunes/albums
 
     :param date:        string date in ISO format %Y-%m-%d
     :param country:     string country code, e.g. 'US'
 
-    :return:            list of dictionary of albums data on iTunes charts
+    :return:            list of dictionary of albums data
+                        on iTunes charts
     """
     urlhandle = f"{itunes_charts_url}/albums"
     params = {
@@ -26,14 +27,15 @@ def albums(date, country="US"):
 
 def tracks(date, country="US"):
     """
-    Query the charts/itunes/tracks endpoint for the given date.
+    Get the top 200 tracks on itunes charts for the given date.
 
     https://api.chartmetric.com/api/charts/itunes/tracks
 
     :param date:        string date in ISO format %Y-%m-%d
     :param country:     string country code, e.g. 'US'
 
-    :return:            list of dictionary of tracks data on iTunes charts
+    :return:            list of dictionary of tracks data
+                        on iTunes charts
     """
     urlhandle = f"{itunes_charts_url}/tracks"
     params = {
@@ -48,14 +50,15 @@ def tracks(date, country="US"):
 
 def videos(date, country="US"):
     """
-    Query the charts/itunes/videos endpoint for the given date.
+    Get the top 200 videos on iTunes chart for the given date.
 
     https://api.chartmetric.com/api/charts/itunes/videos
 
     :param date:        string date in ISO format %Y-%m-%d
     :param country:     string country code, e.g. 'US'
 
-    :return:            list of dictionary of videos data on iTunes charts
+    :return:            list of dictionary of videos data
+                        on iTunes charts
     """
     urlhandle = f"{itunes_charts_url}/videos"
     params = {
