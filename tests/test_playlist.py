4abf993f812b45e3ec1ced27e3f1f0486af12e24
Richard Shu <richard@musicfox.io>
2019-08-27 19:47:15 +0000

    general clean-up and delete comments that are no longer true

--- tests/test_playlist.py
+++ tests/test_playlist.py
@@ -1,8 +1,3 @@
-"""
-These endpoints do not work as listed on the cm documentation. 04-22-2019.
-https://cm-test-node-api.herokuapp.com/apidoc/#api-Playlist-GetPlaylistMetadata
-"""
-
 import pytest
 import pycm
 
@@ -27,36 +22,25 @@ def dta():
 
 
 def test_metadata(dta):
-    """
-    This endpoint is breaking upstream. 04-22-2019.
-    """
     test = pycm.playlist.metadata(dta['cmid'], dta['stype'])
     assert isinstance(test, type(dict()))
     assert len(test.keys()) > 0
 
 
-def test_evolution(dates):
-    """
-    This endpoint is breaking upstream. 04-22-2019.
-    """
+def test_evolution():
     test = pycm.playlist.evolution(439, 'artist')
-
     assert isinstance(test, type(dict()))
     assert len(test.keys()) > 0
 
 
 def test_lists():
-    """
-    chartmetric.com endpoint error -> the exact given url as "ok"
-    does not work...obviously this test isn't going to pass
-    """
     test = pycm.playlist.lists('spotify', )
     assert isinstance(test, type(list()))
     assert len(test) > 0
 
 
 def test_snapshot(dta, dates):
-    test = pycm.playlist.snapshot(dta['cmid'] ,
+    test = pycm.playlist.snapshot(dta['cmid'],
                                   dta['stype'],
                                   dates['start'])
     assert isinstance(test, type(list()))
