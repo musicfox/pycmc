a33c83dfc289f6d2c6344718892529adf8a61f76
Richard Shu <richard@musicfox.io>
2019-08-27 19:48:18 +0000

    add blank lines

--- tests/test_search_engine.py
+++ tests/test_search_engine.py
@@ -2,9 +2,11 @@ import pytest
 import pycm
 from pycm import utilities
 
+
 @pytest.fixture
 def sr_keys():
     return ['artists', 'playlists', 'tracks', 'curators', 'albums']
+
 
 def test_search(sr_keys):
     test = pycm.search_engine.search('Eminem')
@@ -14,3 +16,4 @@ def test_search(sr_keys):
         assert key in test.keys()
         result_len.append(len(test[key]) > 0)
     assert any(result_len)
+    
