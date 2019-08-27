69bf210a65e75e4e085d432032ffe98f33765966
Richard Shu <richard@musicfox.io>
2019-08-27 19:47:57 +0000

    add blank lines

--- tests/test_recommendation.py
+++ tests/test_recommendation.py
@@ -2,7 +2,9 @@ import pytest
 import pycm
 from pycm import utilities
 
+
 def test_recommendation_similar_playlists():
     test = pycm.recommendation.similar_playlists('68810', 'spotify')
     assert isinstance(test, type(list()))
     assert len(test) > 0
+    
