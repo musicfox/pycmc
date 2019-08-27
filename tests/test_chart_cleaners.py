fd158d994fc11ff469dc0b8e17c878a8eb462fdd
Richard Shu <richard@musicfox.io>
2019-08-27 17:58:46 +0000

    general clean-up of tests and long lines

--- tests/test_chart_cleaners.py
+++ tests/test_chart_cleaners.py
@@ -11,9 +11,11 @@ def platforms():
     """
     return ['youtube', 'itunes', 'spotify', 'applemusic', 'shazam']
 
+
 @pytest.fixture
 def date():
     return '2019-01-10'
+
 
 @pytest.fixture
 def cm_raw(platforms, date):
@@ -35,6 +37,7 @@ def cm_raw(platforms, date):
         raw_dicts[platform] = res
     return raw_dicts
 
+
 @pytest.fixture
 def parsed_df(cm_raw, date):
     """
@@ -49,47 +52,62 @@ def parsed_df(cm_raw, date):
 
 def test_cm_raw_keys(cm_raw):
     """
-    Test if the dictionaries within the list of raw cm pulls contain all the expected data fields as keys.
+    Test if the dictionaries within the list of raw CM pulls
+    contain all the expected data fields as keys.
     """
-    youtube_keys = ['added_at', 'artist_covers', 'artist_images', 'artist_name', 'artist_names',
-                    'cm_artist', 'cm_track', 'code2s', 'id', 'image_url', 'isrc', 'name',
-                    'peak_date', 'peak_rank', 'position', 'pre_rank', 'rankStats', 'raw_data',
-                    'time_on_chart', 'view_count', 'youtube_artist', 'youtube_artist_ids',
-                    'youtube_artist_names', 'youtube_track_id', 'youtube_track_ids']
+    youtube_keys = [
+        'added_at', 'artist_covers', 'artist_images', 'artist_name',
+        'artist_names', 'cm_artist', 'cm_track', 'code2s', 'id',
+        'image_url', 'isrc', 'name', 'peak_date', 'peak_rank',
+        'position', 'pre_rank', 'rankStats', 'raw_data', 'time_on_chart',
+        'view_count', 'youtube_artist', 'youtube_artist_ids',
+        'youtube_artist_names', 'youtube_track_id', 'youtube_track_ids',
+        ]
 
-    apple_keys = ['added_at', 'album_ids', 'album_label', 'album_names', 'album_upc',
-                  'artist_covers', 'artist_images', 'artist_names', 'cm_artist', 'cm_track',
-                  'code2', 'code2s', 'composer_name', 'country', 'id', 'image_url', 'isrc',
-                  'itunes', 'itunes_album_id', 'itunes_album_ids', 'itunes_artist_ids',
-                  'itunes_artist_names', 'itunes_track_ids', 'name', 'peak_date', 'peak_rank',
-                  'pre_rank', 'rank', 'rankStats', 'release_dates', 'storefronts',
-                  'time_on_chart', 'track_genre']
+    apple_keys = [
+        'added_at', 'album_ids', 'album_label', 'album_names',
+        'album_upc', 'artist_covers', 'artist_images', 'artist_names',
+        'cm_artist', 'cm_track', 'code2', 'code2s', 'composer_name',
+        'country', 'id', 'image_url', 'isrc', 'itunes', 'itunes_album_id',
+        'itunes_album_ids', 'itunes_artist_ids', 'itunes_artist_names',
+        'itunes_track_ids', 'name', 'peak_date', 'peak_rank', 'pre_rank',
+        'rank', 'rankStats', 'release_dates', 'storefronts', 
+        'time_on_chart', 'track_genre'
+        ]
 
-    itunes_keys = ['added_at', 'album_ids', 'album_label', 'album_names', 'album_upc',
-                   'artist_covers', 'artist_images', 'artist_names', 'cm_artist', 'cm_track',
-                   'code2', 'code2s', 'composer_name', 'genre', 'id', 'image_url', 'isrc',
-                   'itunes', 'itunes_album_id', 'itunes_album_ids', 'itunes_artist_ids',
-                   'itunes_artist_names', 'itunes_track_ids', 'name', 'peak_date', 'peak_rank',
-                   'pre_rank', 'rank', 'rankStats', 'release_dates', 'storefronts',
-                   'time_on_chart', 'track_genre']  
+    itunes_keys = [
+        'added_at', 'album_ids', 'album_label', 'album_names',
+        'album_upc', 'artist_covers', 'artist_images', 'artist_names',
+        'cm_artist', 'cm_track', 'code2', 'code2s', 'composer_name',
+        'genre', 'id', 'image_url', 'isrc', 'itunes', 'itunes_album_id',
+        'itunes_album_ids', 'itunes_artist_ids', 'itunes_artist_names',
+        'itunes_track_ids', 'name', 'peak_date', 'peak_rank', 'pre_rank',
+        'rank', 'rankStats', 'release_dates', 'storefronts', 
+        'time_on_chart', 'track_genre'
+        ]  
     
-    shazam_keys = ['added_at', 'album_ids', 'album_label', 'album_names', 'album_upc',
-                   'artist_covers', 'artist_images', 'artist_names', 'city', 'cm_artist',
-                   'cm_track', 'code2', 'code2s', 'composer_name', 'id', 'image_url', 'isrc',
-                   'itunes_album_id', 'itunes_album_ids', 'itunes_artist_ids',
-                   'itunes_artist_names', 'itunes_id', 'itunes_track_ids', 'name',
-                   'num_of_shazams', 'peak_date', 'peak_rank', 'pre_rank', 'rank', 'rankStats',
-                   'release_dates', 'shazam_track_id', 'storefronts', 'time_on_chart',
-                   'track_genre']    
+    shazam_keys = [
+        'added_at', 'album_ids', 'album_label', 'album_names',
+        'album_upc', 'artist_covers', 'artist_images', 'artist_names',
+        'city', 'cm_artist', 'cm_track', 'code2', 'code2s', 'composer_name',
+        'id', 'image_url', 'isrc', 'itunes_album_id', 'itunes_album_ids',
+        'itunes_artist_ids', 'itunes_artist_names', 'itunes_id', 
+        'itunes_track_ids', 'name', 'num_of_shazams', 'peak_date',
+        'peak_rank', 'pre_rank', 'rank', 'rankStats', 'release_dates',
+        'shazam_track_id', 'storefronts', 'time_on_chart', 'track_genre'
+        ]    
     
-    spotify_keys = ['added_at', 'album_ids', 'album_label', 'album_names', 'album_upc',
-                    'artist_covers', 'artist_images', 'artist_names', 'chart_name',
-                    'chart_type', 'cm_artist', 'cm_track', 'code2', 'code2s',
-                    'current_plays', 'duration', 'id', 'image_url', 'isrc', 'name',
-                    'peak_date', 'peak_rank', 'pre_rank', 'rank', 'rankStats',
-                    'release_dates', 'spotify', 'spotify_album_id', 'spotify_album_ids',
-                    'spotify_artist_ids', 'spotify_artist_names', 'spotify_duration_ms',
-                    'spotify_popularity', 'spotify_track_ids', 'time_on_chart', 'track_genre']
+    spotify_keys = [
+        'added_at', 'album_ids', 'album_label', 'album_names',
+        'album_upc', 'artist_covers', 'artist_images', 'artist_names',
+        'chart_name', 'chart_type', 'cm_artist', 'cm_track', 'code2',
+        'code2s', 'current_plays', 'duration', 'id', 'image_url', 'isrc',
+        'name', 'peak_date', 'peak_rank', 'pre_rank', 'rank', 'rankStats',
+        'release_dates', 'spotify', 'spotify_album_id', 'spotify_album_ids',
+        'spotify_artist_ids', 'spotify_artist_names', 'spotify_duration_ms',
+        'spotify_popularity', 'spotify_track_ids', 'time_on_chart', 
+        'track_genre'
+        ]
 
     for plat, res in cm_raw.items():  
         try:
@@ -111,11 +129,20 @@ def test_cm_raw_keys(cm_raw):
                 except AssertionError:
                     err_count += 1
                     print("Assertion Error!")
-                    print(f"Keys not expected for cm_track {each['cm_track']}-> {set(each.keys()) - set(keys_map[plat])}")
-                    print(f"Keys not present for cm_track {each['cm_track']} -> {set(keys_map[plat]) - set(each.keys())}")
+                    print(
+                        f"Keys not expected for cm_track {each['cm_track']}"
+                        f" -> {set(each.keys()) - set(keys_map[plat])}"
+                    )
+                    print(
+                        f"Keys not present for cm_track {each['cm_track']}"
+                        f" -> {set(keys_map[plat]) - set(each.keys())}"
+                    )
                     print("===========")
 
-            print(f"Total occurrence of errors: {err_count}. Error rate: {err_count/tot_test}.")
+            print(
+                f"Total occurrence of errors: {err_count}."
+                f"Error rate: {err_count/tot_test}."
+            )
 
         except AssertionError:
             print("Invalid input for testing: empty list.")    
@@ -123,53 +150,72 @@ def test_cm_raw_keys(cm_raw):
 
 def test_parse_charts(cm_raw):
     """
-    Test if the parsed DataFrame contains all the expected data fields.
+    Test if the parsed DataFrame contains all
+    the expected data fields.
     """
-    youtube_keys = ['added_at', 'artist_covers_1', 'artist_images_1', 'artist_name',
-                    'artist_names_1', 'cm_artist_1', 'cm_track', 'code2s_1', 'id',
-                    'image_url', 'isrc', 'name', 'peak_date', 'peak_rank', 'position',
-                    'pre_rank', 'rank_1d_ago', 'rank_today', 'percent_views_change',
-                    'time_on_chart', 'view_count', 'youtube_artist', 'youtube_artist_ids_1',
-                    'youtube_artist_names_1', 'youtube_track_id', 'youtube_track_ids_1']
+    youtube_keys = [
+        'added_at', 'artist_covers_1', 'artist_images_1',
+        'artist_name', 'artist_names_1', 'cm_artist_1',
+        'cm_track', 'code2s_1', 'id', 'image_url', 'isrc',
+        'name', 'peak_date', 'peak_rank', 'position', 'pre_rank',
+        'rank_1d_ago', 'rank_today', 'percent_views_change',
+        'time_on_chart', 'view_count', 'youtube_artist',
+        'youtube_artist_ids_1', 'youtube_artist_names_1',
+        'youtube_track_id', 'youtube_track_ids_1'
+        ]
 
-    apple_keys = ['added_at', 'album_ids_1', 'album_label_1', 'album_names_1',
-                  'album_upc_1', 'artist_covers_1', 'artist_images_1', 'cm_track',
-                  'artist_names_1', 'cm_artist_1',  'code2', 'code2s_1','itunes', 
-                  'composer_1', 'country', 'id', 'image_url', 'isrc', 'name', 
-                  'itunes_album_id_1', 'itunes_album_ids_1', 'itunes_artist_ids_1',
-                  'itunes_artist_names_1', 'itunes_track_ids_1', 'peak_date',
-                  'peak_rank', 'pre_rank', 'rank', 'rank_1d_ago', 'rank_today',
-                  'release_dates_1', 'storefronts_1', 'time_on_chart', 'track_genre_1']
+    apple_keys = [
+        'added_at', 'album_ids_1', 'album_label_1', 'album_names_1',
+        'album_upc_1', 'artist_covers_1', 'artist_images_1',
+        'cm_track', 'artist_names_1', 'cm_artist_1',  'code2',
+        'code2s_1', 'itunes', 'composer_1', 'country', 'id',
+        'image_url', 'isrc', 'name', 'itunes_album_id_1',
+        'itunes_album_ids_1', 'itunes_artist_ids_1', 
+        'itunes_artist_names_1', 'itunes_track_ids_1', 'peak_date', 
+        'peak_rank', 'pre_rank', 'rank', 'rank_1d_ago',
+        'rank_today', 'release_dates_1', 'storefronts_1',
+        'time_on_chart', 'track_genre_1'
+        ]
 
-    itunes_keys = ['added_at', 'album_ids_1', 'album_label_1', 'album_names_1',
-                   'album_upc_1', 'artist_covers_1', 'artist_images_1', 'cm_track',
-                   'artist_names_1', 'cm_artist_1', 'code2', 'code2s_1', 'genre', 
-                   'composer_1', 'id', 'image_url', 'isrc', 'itunes',
-                   'itunes_album_id_1', 'itunes_album_ids_1', 'itunes_artist_ids_1',
-                   'itunes_artist_names_1', 'itunes_track_ids_1', 'name', 'peak_date',
-                   'peak_rank', 'pre_rank', 'rank', 'rank_1d_ago', 'rank_today',
-                   'release_dates_1', 'storefronts_1', 'time_on_chart', 'track_genre_1']  
+    itunes_keys = [
+        'added_at', 'album_ids_1', 'album_label_1', 'album_names_1',
+        'album_upc_1', 'artist_covers_1', 'artist_images_1',
+        'cm_track', 'artist_names_1', 'cm_artist_1', 'code2',
+        'code2s_1', 'genre', 'composer_1', 'id', 'image_url',
+        'isrc', 'itunes', 'itunes_album_id_1', 'itunes_album_ids_1',
+        'itunes_artist_ids_1', 'itunes_artist_names_1',
+        'itunes_track_ids_1', 'name', 'peak_date', 'peak_rank',
+        'pre_rank', 'rank', 'rank_1d_ago', 'rank_today', 
+        'release_dates_1', 'storefronts_1', 'time_on_chart',
+        'track_genre_1'
+        ]  
     
-    shazam_keys = ['added_at', 'album_ids_1', 'album_label_1', 'album_names_1',
-                   'album_upc_1', 'artist_covers_1', 'artist_images_1', 'cm_track', 
-                   'artist_names_1', 'city', 'cm_artist_1', 'code2', 'code2s_1',
-                   'composer_1', 'id', 'image_url', 'isrc', 'itunes_album_id_1',
-                   'itunes_album_ids_1', 'itunes_artist_ids_1', 'itunes_artist_names_1',
-                   'itunes_id', 'itunes_track_ids_1', 'name', 'num_of_shazams', 'peak_date',
-                   'peak_rank', 'pre_rank', 'rank', 'rank_1d_ago', 'rank_today',
-                   'release_dates_1', 'shazam_track_id', 'storefronts_1', 'time_on_chart',
-                   'track_genre_1']    
+    shazam_keys = [
+        'added_at', 'album_ids_1', 'album_label_1', 'album_names_1', 
+        'album_upc_1', 'artist_covers_1', 'artist_images_1',
+        'cm_track', 'artist_names_1', 'city', 'cm_artist_1', 'code2',
+        'code2s_1', 'composer_1', 'id', 'image_url', 'isrc',
+        'itunes_album_id_1', 'itunes_album_ids_1',
+        'itunes_artist_ids_1', 'itunes_artist_names_1', 'itunes_id',
+        'itunes_track_ids_1', 'name', 'num_of_shazams', 'peak_date', 
+        'peak_rank', 'pre_rank', 'rank', 'rank_1d_ago', 'rank_today',
+        'release_dates_1', 'shazam_track_id', 'storefronts_1',
+        'time_on_chart', 'track_genre_1'
+        ]    
     
-    spotify_keys = ['added_at', 'album_ids_1', 'album_label_1', 'album_names_1',
-                    'album_upc_1', 'artist_covers_1', 'artist_images_1',
-                    'artist_names_1', 'chart_name', 'chart_type', 'cm_artist_1',
-                    'cm_track', 'code2', 'code2s_1', 'current_plays', 'duration', 'id',
-                    'image_url', 'isrc', 'name', 'peak_date', 'peak_rank', 'pre_rank',
-                    'rank', 'rank_1d_ago', 'rank_today', 'plays_1d_ago', 'plays_today',
-                    'release_dates_1', 'spotify', 'spotify_album_id', 'time_on_chart', 
-                    'spotify_album_ids_1', 'spotify_artist_ids_1', 'track_genre_1',
-                    'spotify_artist_names_1', 'spotify_duration_ms', 'spotify_popularity',
-                    'spotify_track_ids_1']
+    spotify_keys = [
+        'added_at', 'album_ids_1', 'album_label_1', 'album_names_1', 
+        'album_upc_1', 'artist_covers_1', 'artist_images_1',
+        'artist_names_1', 'chart_name', 'chart_type', 'cm_artist_1',
+        'cm_track', 'code2', 'code2s_1', 'current_plays', 'duration',
+        'id', 'image_url', 'isrc', 'name', 'peak_date', 'peak_rank',
+        'pre_rank', 'rank', 'rank_1d_ago', 'rank_today',
+        'plays_1d_ago', 'plays_today', 'release_dates_1', 'spotify',
+        'spotify_album_id', 'time_on_chart', 'spotify_album_ids_1',
+        'spotify_artist_ids_1', 'track_genre_1', 
+        'spotify_artist_names_1', 'spotify_duration_ms',
+        'spotify_popularity', 'spotify_track_ids_1'
+        ]
 
     data_field_map = {
         'youtube': youtube_keys,
@@ -198,12 +244,16 @@ def test_parse_charts(cm_raw):
                 print(f"Column not present in parsed data -> {k}")
                 print("===========")
 
-        print(f"{plat}, {date} -> Total occurrence of errors: {err_count}. Error rate: {err_count/tot_test}.")
+        print(
+            f"{plat}, {date} -> Total occurrence of errors: {err_count}."
+            f"Error rate: {err_count/tot_test}."
+        )
 
 
 def test_type_cast(parsed_df):
     """
-    Test if the type-casted DataFrame has data fields with expected types. 
+    Test if the type-casted DataFrame has data fields
+    with expected types. 
     """
 
     youtube_types = {
@@ -247,7 +297,6 @@ def test_type_cast(parsed_df):
         'cm_artist_1': type(np.float64()),
         'code2': type(str()),
         'code2s_1': type(str()),
-        # 'itunes': type(np.int64()),
         'itunes': type(str()),
         'composer_1': type(str()),
         'country': type(str()),
@@ -285,7 +334,6 @@ def test_type_cast(parsed_df):
         'cm_artist_1': type(np.float64()),
         'code2': type(str()),
         'code2s_1': type(str()),
-        # 'itunes': type(np.int64()),
         'itunes': type(str()),
         'composer_1': type(str()),
         'genre': type(str()),
@@ -323,7 +371,6 @@ def test_type_cast(parsed_df):
         'cm_artist_1': type(np.float64()),
         'code2': type(str()),
         'code2s_1': type(str()),
-        # 'itunes_id': type(np.int64()),
         'itunes_id': type(str()),
         'composer_1': type(str()),
         'city': type(str()),
@@ -336,7 +383,6 @@ def test_type_cast(parsed_df):
         'itunes_artist_ids_1': type(np.float64()),
         'itunes_artist_names_1': type(str()),
         'itunes_track_ids_1': type(np.float64()),
-        # 'shazam_track_id': type(np.int64()),
         'shazam_track_id': type(str()),
         'num_of_shazams': type(np.float64()),
         'peak_date': type(pd.to_datetime('2019-01-01')),
@@ -381,7 +427,6 @@ def test_type_cast(parsed_df):
         'plays_1d_ago': type(np.float64()),
         'plays_today': type(np.float64()),
         'release_dates_1': type(pd.to_datetime('2019-01-01')),
-        # 'spotify': type(np.int64()),
         'spotify': type(str()),
         'spotify_album_id': type(str()),
         'time_on_chart': type(np.float64()), 
@@ -421,11 +466,21 @@ def test_type_cast(parsed_df):
                         assert type(track[k]) == type_map[plat][k]
                     except AssertionError:
                         err_count += 1
-                        print(f"{track['cm_track']}, column: {k} -> expected {type_map[plat][k]}, got {type(track[k])}")
+                        print(
+                            f"{track['cm_track']}, column: {k} -> "
+                            f"expected {type_map[plat][k]},"
+                            f"got {type(track[k])}."
+                        )
 
                 except AssertionError:
-                    print(f"{track['cm_track']}, column: {k} -> key not found or nan encountered.")
+                    print(
+                        f"{track['cm_track']}, column: {k} -> "
+                        f"key not found or nan encountered."
+                    )
 
-        print(f"{plat}, {date} -> Total occurrence of errors: {err_count}. Error rate: {err_count/tot_test}.")
+        print(
+            f"{plat}, {date} -> Total occurrence of errors: {err_count}. "
+            f"Error rate: {err_count/tot_test}."
+        )
         
     
