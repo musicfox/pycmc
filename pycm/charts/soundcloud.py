83b276b3531e650532f8ae833764046918b434e0
Phil <phil@musicfox.io>
2020-05-12 19:16:28 -0500

    :pencil: update docstring #11

--- pycm/charts/soundcloud.py
+++ pycm/charts/soundcloud.py
@@ -5,18 +5,20 @@ soundcloud_charts_url = f"/charts/soundcloud"
 
 def tracks(date, country="US", kind="top", genre="all-music"):
     """
+    # `tracks`
     Get the top 200 tracks on SoundCloud chart for the given date.
     Data available ONLY on Fridays.
 
     https://api.chartmetric.com/api/charts/soundcloud
 
-    :param date:        string date in ISO format %Y-%m-%d
-    :param country:     string country code, e.g. 'US'
-    :param kind:        string 'top' or 'trending'
-    :param genre:       string genre (see CM docs)
+    ## Parameters
+    - `date`:        string date in ISO format %Y-%m-%d
+    - `country`:     string country code, e.g. 'US'
+    - `kind`:        string 'top' or 'trending'
+    - `genre`:       string genre (see CM docs)
 
-    :return:            list of dictionary of tracks on 
-                        SoundCloud chart
+    ## Returns
+    list of dictionary of tracks on SoundCloud chart
     """
     urlhandle = f"{soundcloud_charts_url}"
     params = {
