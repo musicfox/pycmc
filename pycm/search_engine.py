15c8911617062538030ae73b6c5ab3d94a8f71c1
Phil <phil@musicfox.io>
2020-05-12 19:52:31 -0500

    :pencil: update docstring #11

--- pycm/search_engine.py
+++ pycm/search_engine.py
@@ -3,16 +3,19 @@ from . import utilities
 
 def search(query, limit=None, offset=None):
     """
+    # `search`
     Search the tracks, albums, artists, curators and playlists 
     with one single query.
 
     https://api.chartmetric.com/api/search
 
-    :param query:       string of search query, can be URLs
-    :params limit:      int number of entries returned, default 10 
-    :params offset:     int offset of entries returned, default 0
+    ## Parameters
+    - `query`:       string of search query, can be URLs
+    - `limit`:      int number of entries returned, default 10 
+    - `offset`:     int offset of entries returned, default 0
 
-    :returns:           dictionary of results, keys include 
+    ## Returns
+    dictionary of results, keys include 
                         'artists', 'playlists', 'tracks',
                         'curators', 'albums', 'labels',
                         'stations' and 'cities'
