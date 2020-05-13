50d3b8d99a1ca13fe0fd37df7db2491d84aed75e
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2020-05-12 20:40:03 -0500

    :package: :wrench: update for next release and renaming #2

--- setup.py
+++ setup.py
@@ -12,24 +12,13 @@ from shutil import rmtree
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-NAME = 'pycm'
-DESCRIPTION = 'A Python interface for the chartmetric.com API.'
-URL = 'https://github.com/musicfox/pycm'
-EMAIL = 'dev@musicfox.io'
-AUTHOR = 'Jason R. Stevens, CFA | Musicfox Inc.'
-REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.0.1'
-
-# What packages are required for this module to be executed?
-REQUIRED = [
-    # 'requests', 'maya', 'records',
-    'pytest', 'psutil', 'requests',
-]
-
-# What packages are optional?
-EXTRAS = {
-    # 'fancy feature': ['django'],
-}
+NAME = "pycmc"
+DESCRIPTION = "A Python client for the chartmetric.com API."
+URL = "https://github.com/musicfox/pycmc"
+EMAIL = "dev@musicfox.io"
+AUTHOR = "Jason R. Stevens, CFA | Musicfox, Inc. | https://musicfox.io"
+REQUIRES_PYTHON = ">=3.7.0"
+VERSION = "0.0.2"
 
 # The rest you shouldn't have to touch too much :)
 # ------------------------------------------------
@@ -41,8 +30,8 @@ here = os.path.abspath(os.path.dirname(__file__))
 # Import the README and use it as the long-description.
 # Note: this will only work if 'README.md' is present in your MANIFEST.in file!
 try:
-    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
+    with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
+        long_description = "\n" + f.read()
 except FileNotFoundError:
     long_description = DESCRIPTION
 
@@ -50,22 +39,22 @@ except FileNotFoundError:
 about = {}
 if not VERSION:
     project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
-    with open(os.path.join(here, project_slug, '__version__.py')) as f:
+    with open(os.path.join(here, project_slug, "__version__.py")) as f:
         exec(f.read(), about)
 else:
-    about['__version__'] = VERSION
+    about["__version__"] = VERSION
 
 
 class UploadCommand(Command):
     """Support setup.py upload."""
 
-    description = 'Build and publish the package.'
+    description = "Build and publish the package."
     user_options = []
 
     @staticmethod
     def status(s):
         """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
+        print("\033[1m{0}\033[0m".format(s))
 
     def initialize_options(self):
         pass
@@ -75,58 +64,84 @@ class UploadCommand(Command):
 
     def run(self):
         try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
         except OSError:
             pass
 
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+        self.status("Building Source and Wheel (universal) distribution…")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
 
-        self.status('Uploading the package to PyPI via Twine…')
-        os.system('twine upload dist/*')
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
 
-        self.status('Pushing git tags…')
-        os.system('git tag v{0}'.format(about['__version__']))
-        os.system('git push --tags')
-        
+        self.status("Pushing git tags…")
+        os.system("git tag v{0}".format(about["__version__"]))
+        os.system("git push --tags")
+
         sys.exit()
 
 
 # Where the magic happens:
 setup(
     name=NAME,
-    version=about['__version__'],
+    version=about["__version__"],
     description=DESCRIPTION,
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     author=AUTHOR,
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    packages=find_packages(exclude=('tests',)),
+    packages=find_packages(exclude=("tests",)),
     # If your package is a single module, use this instead of 'packages':
     # py_modules=['mypackage'],
-
     # entry_points={
     #     'console_scripts': ['mycli=mymodule:cli'],
     # },
-    install_requires=REQUIRED,
-    extras_require=EXTRAS,
+    install_requires=[
+        "attrs==19.3.0",
+        "certifi==2020.4.5.1",
+        "chardet==3.0.4",
+        "coverage==5.1",
+        "idna==2.9",
+        "importlib-metadata==1.6.0; python_version < '3.8'",
+        "more-itertools==8.2.0",
+        "numpy==1.18.4",
+        "packaging==20.3",
+        "pandas==1.0.3",
+        "pluggy==0.13.1",
+        "psutil==5.7.0",
+        "py==1.8.1",
+        "pyparsing==3.0.0a1",
+        "pytest==5.4.2",
+        "pytest-cov==2.8.1",
+        "python-dateutil==2.8.1",
+        "pytz==2020.1",
+        "requests==2.23.0",
+        "six==1.14.0",
+        "urllib3==1.25.9",
+        "wcwidth==0.1.9",
+        "zipp==3.1.0",
+    ],
     include_package_data=True,
-    license='MIT',
+    license="MIT",
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy'
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
     ],
     # $ setup.py publish support.
-    cmdclass={
-        'upload': UploadCommand,
-    },
+    cmdclass={"upload": UploadCommand,},
+    project_urls={
+        "Documentation": "https://pycm.docs.musicfox.io",
+        "Bug Reports": "https://github.com/musicfox/pycmc/issues",
+        "Source Code": "https://github.com/musicfox/pycmc",
+        "Musicfox": "https://musicfox.io",
+    }
 )
