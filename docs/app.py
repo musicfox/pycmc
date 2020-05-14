dea49c5d65d8f6e330b6b4913d1486b2be013fc6
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2020-05-13 22:45:43 -0500

    :recycle: add google exemplar gunicorn call

--- docs/app.py
+++ docs/app.py
@@ -6,15 +6,12 @@ Main Dash/Flask application code.
 import flask
 from flask import Flask
 
-app = Flask(
-    __name__,
-    static_url_path = '/', 
-    static_folder='_build/html',
-)
+app = Flask(__name__, static_url_path="/", static_folder="_build/html",)
 
-@app.route('/')
-@app.route('/<path:path>')
-def serve_documentation(path = 'index.html'):
+
+@app.route("/")
+@app.route("/<path:path>")
+def serve_documentation(path="index.html"):
     """
     Serve the Sphinx generated html documentation
     located in the doc/_build/html directory.
@@ -27,4 +24,4 @@ def serve_documentation(path = 'index.html'):
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
--- docs/conf.py
+++ docs/conf.py
@@ -12,15 +12,16 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
+
+sys.path.insert(0, os.path.abspath("../"))
 sys.setrecursionlimit(1500)
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'pycmc'
-copyright = '2020, Musicfox, Inc. | musicfox.io'
-author = 'Jason R. Stevens, CFA'
+project = "pycmc"
+copyright = "2020, Musicfox, Inc. | musicfox.io"
+author = "Jason R. Stevens, CFA"
 
 
 # -- General configuration ---------------------------------------------------
@@ -29,21 +30,21 @@ author = 'Jason R. Stevens, CFA'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-    'recommonmark',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.coverage',
-    'sphinx.ext.todo',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "recommonmark",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.coverage",
+    "sphinx.ext.todo",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -51,42 +52,48 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_logo = '_static/logo-dark-text.png'
-html_favicon = '_static/favicon.ico'
-html_theme = 'pydata_sphinx_theme'
+html_logo = "_static/logo-dark-text.png"
+html_favicon = "_static/favicon.ico"
+html_theme = "pydata_sphinx_theme"
 html_theme_options = {
-    "external_links": [
-        {"name": "Musicfox", "url": "https://musicfox.io"},
-    ],
+    "external_links": [{"name": "Musicfox", "url": "https://musicfox.io"},],
     "github_url": "https://github.com/musicfox/pycmc",
     "twitter_url": "https://twitter.com/musicfoxinc",
     "search_bar_text": "Search pycmc docs ...",
 }
-html_sidebars = {'**': ['util/searchbox.html', 'util/sidetoc.html', 'sourcelink.html', 'relations.html']}
-
+html_sidebars = {
+    "**": [
+        "util/searchbox.html",
+        "util/sidetoc.html",
+        "sourcelink.html",
+        "relations.html",
+    ]
+}
 
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # Add basic extension to take python docstrings
 # autodoc
 
 # -- Options for Syntax Highlighting -----------------------------------------
-pygments_style = 'colorful'
+pygments_style = "colorful"
 
 # -- Markdown support --------------------------------------------------------
 # https://stackoverflow.com/a/56428123/5369706
 import commonmark
 
+
 def docstring(app, what, name, obj, options, lines):
-    md  = '\n'.join(lines)
+    md = "\n".join(lines)
     ast = commonmark.Parser().parse(md)
     rst = commonmark.ReStructuredTextRenderer().render(ast)
     lines.clear()
     lines += rst.splitlines()
 
+
 def setup(app):
-    app.connect('autodoc-process-docstring', docstring)
+    app.connect("autodoc-process-docstring", docstring)
