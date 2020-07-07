89b0433bb6e70462f4b17ab01e7320068f048c07
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2020-07-07 16:43:07 -0500

    :package: bump version to 0.0.6

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = "https://github.com/musicfox/pycmc"
 EMAIL = "dev@musicfox.io"
 AUTHOR = "Jason R. Stevens, CFA | Musicfox, Inc. | https://musicfox.io"
 REQUIRES_PYTHON = ">=3.8.0"
-VERSION = "0.0.5"
+VERSION = "0.0.6"
 
 # The rest you shouldn't have to touch too much :)
 # ------------------------------------------------
