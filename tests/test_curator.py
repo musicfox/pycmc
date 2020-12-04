f9693f7348adb3e6a5d7821335d316194f07b49e
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2020-12-03 20:28:27 -0600

    :recycle: refactor isinstance usage and get_playlist returns dict()

--- tests/test_curator.py
+++ tests/test_curator.py
@@ -4,17 +4,17 @@ import pycmc
 
 def test_lists():
     test = pycmc.curator.lists("spotify",)
-    assert isinstance(test, type(list()))
+    assert isinstance(test, list)
     assert len(test) > 0
 
 
 def test_metadata(dates):
     test = pycmc.curator.metadata("132426", "spotify")
-    assert isinstance(test, type(dict()))
+    assert isinstance(test, dict)
     assert len(test.keys()) > 0
 
 
 def test_playlists(dates):
     test = pycmc.curator.playlists("132426", "spotify")
-    assert isinstance(test, type(list()))
+    assert isinstance(test, dict)
     assert len(test) > 0
